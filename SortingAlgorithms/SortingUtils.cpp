685b045db29dfca7a88319fba412f4095857ee4e
Christian Bauer <macbook@Christians-MacBook-Pro.local>
2019-10-15 17:45:32 +0200

    Further work on SortingUtils

--- SortingAlgorithms/SortingUtils.cpp
+++ SortingAlgorithms/SortingUtils.cpp
@@ -9,7 +9,20 @@ void swap(int *a, int *b)
     *b = temp;
 }
 
-void printArray(int *array)
+void print_vector(vector<int> vec)
 {
-    int length = sizeof(array) / sizeof(*array);
+    using namespace std;
+    for (int i = 0; i < vec.size(); i++)
+        cout << vec[i] << " ";
+
+    cout << endl;
+}
+
+bool is_sorted(vector<int> vec)
+{
+    for (int i = 0; i < vec.size(); i++)
+        if (vec[i] > vec[i + 1]) // we found an unsorted position
+            return false;
+
+    return true; // list is sorted
 }
--- SortingAlgorithms/SortingUtils.h
+++ SortingAlgorithms/SortingUtils.h
@@ -1,14 +1,26 @@
 #ifndef SORTING_UTILS_H
 #define SORTING_UTILS_H
 
+#include <vector>
+
+using namespace std;
+
+
 /**
- * Swaps the values in the memory addresses of given parameters
+ * Swaps the values in the memory addresses of given arguments
  */
 void swap(int *a, int *b);
 
 /**
  * Prints the given array on the console
  */
-void printArray(int *array);
+void print_vector(vector<int> vec);
+
+/**
+ * Checks if given vector is sorted
+ * 
+ * @return true, if sorted, else false
+ */
+bool is_sorted(vector<int> vec);
 
 #endif
