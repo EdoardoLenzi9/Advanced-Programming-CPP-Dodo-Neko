ba71e5e42e9b9ccd34ce44deaa53729a63cf3f5b
Christian Bauer <macbook@Christians-MacBook-Pro.local>
2019-10-15 22:06:09 +0200

    Adding SortingUtils

--- SortingAlgorithms/SortingUtils.cpp
+++ SortingAlgorithms/SortingUtils.cpp
@@ -1,28 +1,55 @@
-#include <iostream>
+#include "SortingUtils.hpp"
 
-#include "SortingUtils.h"
+std::random_device rd;  //Will be used to obtain a seed for the random number engine
+std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
 
-void swap(int *a, int *b)
+void print_vector(const vector<int> &vec)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-void print_vector(vector<int> vec)
-{
-    using namespace std;
     for (int i = 0; i < vec.size(); i++)
         cout << vec[i] << " ";
 
     cout << endl;
 }
 
-bool is_sorted(vector<int> vec)
+void print_vector(const vector<int> *vec)
 {
-    for (int i = 0; i < vec.size(); i++)
-        if (vec[i] > vec[i + 1]) // we found an unsorted position
+    for (int i = 0; i < vec->size(); i++)
+        cout << vec->at(i) << " ";
+
+    cout << endl;
+}
+
+bool is_sorted(const vector<int> &vec)
+{
+    for (int i = 0; i < vec.size() - 1; i++)
+        if (vec[i] > vec[i + 1]) // unsorted elements found
             return false;
 
-    return true; // list is sorted
+    return true; // vector is sorted
+}
+
+bool is_sorted(const vector<int> *vec)
+{
+    for (int i = 0; i < vec->size() - 1; i++)
+        if (vec->at(i) > vec->at(i + 1)) // unsorted elements found
+            return false;
+
+    return true; // vector is sorted
+}
+
+void fill_vector(vector<int> &vec, int from, int to)
+{
+    std::uniform_int_distribution<> dis(from, to);
+
+    for (int i = 0; i < vec.capacity(); i++)
+
+        vec.push_back(dis(gen));
+}
+
+void fill_vector(vector<int> *vec, int from, int to)
+{
+    std::uniform_int_distribution<> dis(from, to);
+
+    for (int i = 0; i < vec->capacity(); i++)
+        vec->push_back(dis(gen));
 }
--- SortingAlgorithms/SortingUtils.hpp
+++ SortingAlgorithms/SortingUtils.hpp
@@ -2,25 +2,60 @@
 #define SORTING_UTILS_HPP
 
 #include <vector>
+#include <iostream>
+#include <random>
 
 using namespace std;
 
 
 /**
- * Swaps the values in the memory addresses of given arguments
+ * Prints the given vector on the console
+ * 
+ * Function for statically created vectors
  */
-void swap(int *a, int *b);
+void print_vector(const vector<int> &vec);
 
 /**
- * Prints the given array on the console
+ * Prints the given vector on the console
+ * 
+ * Function for dynamically created vectors
  */
-void print_vector(vector<int> vec);
+void print_vector(const vector<int> *vec);
 
 /**
  * Checks if given vector is sorted
  * 
+ * Function for statically created vectors
+ * 
  * @return true, if sorted, else false
  */
-bool is_sorted(vector<int> vec);
+bool is_sorted(const vector<int> &vec);
+
+/**
+ * Checks if given vector is sorted
+ * 
+ * Function for dynamically created vectors
+ * 
+ * @return true, if sorted, else false
+ */
+bool is_sorted(const vector<int> *vec);
+
+/**
+ * Fills a given statically created vector with randomized values (uniformly distributed)
+ * 
+ * @param vec   - vector to be filled
+ * @param from  - start range of random values
+ * @param to    - end range of random values
+ */
+void fill_vector(vector<int> &vec, int from, int to);
+
+/**
+ * Fills a given statically created vector with randomized values (uniformly distributed)
+ * 
+ * @param vec   - vector to be filled
+ * @param from  - start range of random values
+ * @param to    - end range of random values
+ */
+void fill_vector(vector<int> *vec, int from, int to);
 
 #endif
