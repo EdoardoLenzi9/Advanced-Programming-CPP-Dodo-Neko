61f8d8f1d3ab61385786d0ed58e26db98f7ea696
Christian Bauer <macbook@Christians-MacBook-Pro.local>
2019-10-16 21:27:23 +0200

    Small changes in BubbleSort files

--- SortingAlgorithms/BubbleSort/BubbleSort.hpp
+++ SortingAlgorithms/BubbleSort/BubbleSort.hpp
@@ -2,12 +2,13 @@
 #define BUBBLESORT_HPP
 
 #include <vector>
+#include "../SortingUtils.hpp"
 
 using namespace std;
 
 class BubbleSort {
-    public:
-        void sort(vector<int> &vec);
+public:
+    void sort(vector<int> &vec);
 };
 
 #endif
