e8996a0557a9de3977d7b757c26d14a594bb0f3a
Christian Bauer <macbook@Christians-MacBook-Pro.local>
2019-10-17 18:29:39 +0200

    Adding SortingInterface

---
+++ SortingAlgorithms/SortingInterface.hpp
@@ -0,0 +1,16 @@
+#ifndef SORTING_INTERFACE_HPP
+#define SORTING_INTERFACE_HPP
+
+#include <vector>
+
+using namespace std;
+
+class SortingInterface {
+public:
+  template <typename T>
+  void sort(vector<T> &vec);
+  template <typename T>
+  void sort(vector<T> *vec);
+};
+
+#endif
