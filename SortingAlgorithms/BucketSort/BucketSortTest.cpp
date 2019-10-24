82c56b5ed1099aba5015d28087b4fdef9c0b3a6d
Christian Bauer <macbook@Christians-MacBook-Pro.local>
2019-10-24 19:03:24 +0200

    added missing includes (no traversial dependencies now) or changed includes

--- SortingAlgorithms/BucketSort/BucketSortTest.cpp
+++ SortingAlgorithms/BucketSort/BucketSortTest.cpp
@@ -1,7 +1,14 @@
-#include "BucketSort.hpp"
-#include "../SortingUtils.hpp"
+#include <vector>
+#include <iostream>
 
-int main(void) {
+#include "BucketSort.hpp"
+#include "SortingUtils.hpp"
+
+int main(void)
+{
+
+    using namespace std;
+
     vector<int> vec;
     vec.reserve(100);
     fill_vector(vec, 0, 100);
@@ -10,16 +17,8 @@ int main(void) {
     BucketSort bs;
     bs.sort(vec);
     print_vector(vec);
-    cout << "Vector sorted: " << is_sorted(vec) << endl << endl;
+    cout << "Vector sorted: " << is_sorted(vec) << endl
+         << endl;
 
-
-    vector<int> * dyn_vec = new vector<int>();
-    dyn_vec->reserve(100);
-    fill_vector(dyn_vec, 0, 100);
-    print_vector(dyn_vec);
-    cout << endl;
-    bs.sort(dyn_vec);
-    print_vector(dyn_vec);
-    cout << is_sorted(dyn_vec) << endl;
     return 0;
 }
