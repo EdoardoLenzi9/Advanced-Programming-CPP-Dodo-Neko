7fbe86ef708ab9db9c71651daebdbe2ab26da320
Christian Bauer <macbook@Christians-MacBook-Pro.local>
2019-10-24 19:02:33 +0200

    moved implementation to header file

--- SortingAlgorithms/BucketSort/BucketSort.hpp
+++ SortingAlgorithms/BucketSort/BucketSort.hpp
@@ -3,7 +3,7 @@
 
 #include <vector>
 
-#include "../SortingUtils.hpp"
+#include "SortingUtils.hpp"
 
 using namespace std;
 
@@ -13,12 +13,23 @@ public:
     /**
      * Sorts the statically created vector with the bucket sort algorithm
      */
-    void sort(vector<int> &vec);
+    void sort(vector<int> &vec)
+    {
+        const int MAX = get_max(vec);
 
-    /**
-     * Sorts the dynamically created vector with the bucket sort algorithm
-     */
-    void sort(vector<int> *vec);
+        // fill the bucket with (max + 1) zeros
+        vector<int> buckets(MAX + 1, 0);
+
+        for (int i = 0; i < vec.size(); i++)
+            buckets[vec[i]]++; // counts the occurence of a number
+
+        for (int i = 0, j = 0; i <= MAX; i++)
+            while (buckets[i])
+            {
+                vec[j++] = i; // add next sorted value
+                buckets[i]--; // decrement the occurence of this value
+            }
+    }
 };
 
 #endif
