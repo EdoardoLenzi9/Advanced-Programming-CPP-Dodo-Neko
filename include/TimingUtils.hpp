c53b10ad0dc4b6ff3a99cdb88834755538040b6c
Christian Bauer <macbook@Christians-MacBook-Pro.local>
2019-10-27 14:25:20 +0100

    Added format_duration function to TimingUtils

--- include/TimingUtils.hpp
+++ include/TimingUtils.hpp
@@ -4,11 +4,14 @@
 #include <chrono> // execution time measurement
 #include <limits> // used for double max value
 #include <string>
+#include <sstream> // std::stringstream
 
 typedef std::chrono::high_resolution_clock::time_point TimeVar;
 
 #define duration(a) std::chrono::duration_cast<std::chrono::microseconds>(a).count()
 #define time_now() std::chrono::high_resolution_clock::now()
+
+using namespace std::chrono;
 
 struct TimeUnits
 {
@@ -37,4 +40,20 @@ void print_units(const TimeUnits &units, string message)
          << " AVERAGE: " << units.average << " microseconds"
          << endl;
 }
+
+string format_duration(std::chrono::milliseconds ms)
+{
+    using namespace std::chrono;
+    auto secs = duration_cast<seconds>(ms);
+    ms -= duration_cast<milliseconds>(secs);
+    auto mins = duration_cast<minutes>(secs);
+    secs -= duration_cast<seconds>(mins);
+
+    std::stringstream ss;
+    if (mins.count() > 0)
+        ss << mins.count() << " minutes\t";
+    ss << secs.count() << " seconds\t" << ms.count() << " milliseconds\t";
+    return ss.str();
+}
+
 #endif
