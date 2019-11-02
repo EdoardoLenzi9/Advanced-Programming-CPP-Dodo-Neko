397de8e0616c11a8267945b73b239892783137b1
EdoardoLenzi <edoardolenzi9@gmail.com>
2019-11-02 15:55:35 +0100

    makefile

--- Homeworks/Homework2/driver.cxx
+++ Homeworks/Homework2/driver.cxx
@@ -1,70 +1,23 @@
-#include <unistd.h> // sleep()
 #include <iostream>
-#include <string>
+
 #include <odb/database.hxx>
 #include <odb/transaction.hxx>
 #include <odb/schema-catalog.hxx>
 #include <odb/sqlite/database.hxx>
 
-#include <iostream>
-#include "Repositories/Entities/User-odb.hxx"
-#include "Repositories/Entities/User.hxx"
-#include "Repositories/Entities/UserBook-odb.hxx"
-#include "Repositories/Entities/UserBook.hxx"
-#include "Repositories/Entities/Role-odb.hxx"
-#include "Repositories/Entities/Role.hxx"
-#include "Repositories/Entities/Book-odb.hxx"
-#include "Repositories/Entities/Book.hxx"
+#include "Repository.hxx"
+#include "User.hxx"
+#include "User-odb.hxx"
 
-using namespace std;
 using namespace odb::core;
- 
-int main ()
-{
-  odb::sqlite::database db (
-    "mytest.db",
-    SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE);
- 
-  // Create the database schema.
-  //
-  {
-    transaction t (db.begin ());
-    schema_catalog::create_schema (db);
-    t.commit ();
-  }
 
-  {
-    transaction t (db.begin ());
-    User u ("Dodo", "Neko");
-    db.persist ( u );
-    t.commit ();
-  }
-
-  //
-  //
-  typedef odb::query<User> query;
-  typedef odb::result<User> result;
-
-  // Find all the transactions involving AMD.
-  //
-  {
-    transaction t (db.begin ());
-
-    result r (db.query<User> (query::name == "Dodo"));
-
-    for (result::iterator i (r.begin ()); i != r.end (); ++i)
-      cerr << *i << endl;
-
-    t.commit ();
-  }
-
-  {
-    transaction t (db.begin ());
-
-    auto_ptr<User> dodo (db.load<User> (1)); //user id
-    dodo->name = "Updated name";
-    db.update(*dodo);
-
-    t.commit ();
-    }
+int main(){
+    Repository<User>* r = new Repository<User>();
+    User* u = new User("Dodo", "Neko");
+    User* u2 = r->create(u);
+    User* u3 = r->read(u2->id());
+    u3->name("Dodo2");
+    r->update(u3);
+    User* u4 = r->read(u3->id());
+    cout << u4->name();
 }
