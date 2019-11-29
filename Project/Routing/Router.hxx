fdb550e9030cd757f0f39d04a8d77400449d036d
Dominik Simon <admin@dsimon.xyz>
2019-11-29 09:11:21 +0100

    moved Route class to own file

--- Project/Routing/Router.hxx
+++ Project/Routing/Router.hxx
@@ -1,37 +1,7 @@
 #ifndef ROUTER_HXX
 #define ROUTER_HXX
 
-#include "client_http.hpp"
-#include "server_http.hpp"
-
-#define BOOST_SPIRIT_THREADSAFE
-#include <boost/property_tree/json_parser.hpp>
-#include <boost/property_tree/ptree.hpp>
-
-#include <algorithm>
-#include <boost/filesystem.hpp>
-#include <fstream>
-#include <vector>
-#include <string>
-
-
-using namespace std;
-using namespace boost::property_tree;
-using HttpServer = SimpleWeb::Server<SimpleWeb::HTTP>;
-
-
-class Route { 
-    
-    public:
-        Route( string path, 
-               string httpMethod, 
-               void (*handler)(shared_ptr<HttpServer::Response>, shared_ptr<HttpServer::Request>))
-               : path(path), httpMethod(httpMethod), handler(handler) {};
-
-        string path;
-        string httpMethod;
-        void (*handler)(shared_ptr<HttpServer::Response>, shared_ptr<HttpServer::Request>);
-};
+#include "Route.hxx"
 
 
 class Router { 
