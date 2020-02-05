c947f58b13fef632d2807a25b4a0cb35cfc22c52
Neko <neko@koneko.at>
2020-02-05 21:42:50 +0100

    added rest api changelog, added new rest api version, implemented ergister and user settings

---
+++ Project/REST-API-Documentation/REST API Changelog.md
@@ -0,0 +1,21 @@
+# REST API Changelog
+
+## Release v.1.1
+
+### Additions
+
+* New endpoint requirement: `/book/<id>`, which allows accessing information for a specific booking.
+* New endpoint requirement: `/meta/api_ver`, which returns the current API version the server is running
+* Added a list of common server response codes, mostly according to official [HTTP status code](https://en.wikipedia.org/wiki/List_of_HTTP_status_codes) conventions 
+
+### Changes
+
+* `/book/list` should now return a list of book-IDs depending on the user
+* `/user` should now also return the `userId` and the `roleId` of the querying user
+* `/user/update` now also contains the `userId` and the `roleId` fields. For customer and staff members, these fields should be ignored (don't allow users to change their own Ids). For Admins the fields should be updated correspondingly
+* `/user/auth` now needs to return a SID
+* `/user/update` no longer returns `successful` in the response data. Use the status code of the default response instead.
+
+## Release v1.0
+
+Initial Release
--- Project/jekyll/assets/js/ui.js
+++ Project/jekyll/assets/js/ui.js
@@ -1,13 +1,19 @@
-// seriously, fuck this language
+///////////////////////////////
+/////// local variables ///////
+///////////////////////////////
 
 
 // please change this to your server
 var serverUrl = "http://localhost:8080"
 
-var userId = 0;
-var roleId = 0;
+// filled via rest api
 var loggedIn = 0;
 
+////////////////////////////////
+// data objects for local use //
+////////////////////////////////
+
+// constants 
 
 const defaultRequest = {
 	"auth": {
@@ -16,6 +22,22 @@ const defaultRequest = {
 	"data": {
 	}
 };
+
+// filled via rest api
+
+var user = {
+	"firstname": "",
+	"lastname": "",
+	"email": "",
+	"birthday": "",
+	"address": "",
+	"userid": "",
+	"roleid": ""
+};
+
+///////////////////////////////
+/////// local functions ///////
+///////////////////////////////
 
 // shows different navbar items depending on the group of the user.
 // potentially a security issue, but pratically, api calls from users
@@ -36,13 +58,13 @@ function showElementsbyState(){
 	if (loggedIn > 0){
 		$('#nav-register').addClass('d-none');
 
-		if (roleId > 1){
+		if (user.roleid > 1){
 			$('#nav-settings').removeClass('d-none');
 		}
-		if (roleId > 2){
+		if (user.roleid > 2){
 			$('#nav-manage').removeClass('d-none');
 		}
-		if (roleId > 3){
+		if (user.roleid > 3){
 			$('#nav-admin').removeClass('d-none');
 			$('#nav-debug').removeClass('d-none');
 		}
@@ -70,6 +92,31 @@ $.postJSON = function(url, data, callback) {
 	});
 };
 
+function loadSettings(){
+	if (! loggedIn){
+		loadMainPage()
+	}
+	getUserInfo();
+
+	$('#frm-email').text("user.email")
+
+	$('#frm-firstname').text(user.firstname);
+	$('#frm-lastname').text(user.lastname);
+	$('#frm-birthday').text(user.birthday);
+	$('#frm-userid').text(user.userid);
+	$('#frm-roleid').text(user.roleid);
+
+	if (user.roleid > 1){
+		$('#frm-userid').attr('disabled', false);
+		$('#frm-roleid').attr('disabled', false);
+
+	}
+}
+
+function loadMainPage(){
+	window.location = window.location.origin;
+}
+
 /*
 	Implementation of the /hotel endpoint
 */
@@ -89,22 +136,28 @@ function getHotelInfo(){
 
 /* 
 	Implementation of the /user endpoint
-
-	TODO: what happens if no user is logged in?
 */
 
 function getUserInfo(){
-	$.getJSON(`${serverUrl}/user`,
-		function(response) {
+	var request = defaultRequest;
+	request.auth.sid = Cookies.get('sid');
+
+	$.postJSON(`${serverUrl}/user`,
+	request,
+	function(response) {
+		if ( response.status.code == "200" ){
+			user.firstname = response.data.firstName;
+			user.lastname = response.data.lastName;
+			user.email = response.data.email;
+			user.birthday = response.data.birthday;
+			user.address = response.data.address;
+			user.userid = response.data.userId;
+			user.roleid = response.data.roleId;
+		} else {
+			console.log("acquiring user info failed");
 			console.log(response);
-
-			$('#userfirstName').text(response.data.firstName);
-			$('#userlastName').text(response.data.lastName);
-			$('#useremail').text(response.data.emailName);
-			$('#userbirthdate').text(response.data.birthday);
-			$('#useraddress').text(response.data.address);
-
- 		 });
+		}
+ 	});
 }
 
 /*
@@ -122,10 +175,20 @@ function login(){
 	$.postJSON(`${serverUrl}/user/auth`,
 	request, 
 	function(response) {
-		if (Cookies.get('sid') == null) {
-				Cookies.set('sid', response.auth.sid, { expires: 7, path: '' }); // 7 days
+		if (response.status.code == "200") {
+			console.log("authentication successful");
+			console.log(response.data.sid);
+			Cookies.set('sid', response.data.sid, { expires: 7, path: '' }); // 7 days	
+		} else if (response.status.code == "401") {
+			// TODO: needs a nicer way of displaying this, maybe some shaking-animation on the login menu
+			alert("authentication failed");
+		} else {
+			console.log("login failed")
+			console.log(response);
 		}
 	});
+	getUserInfo();
+	showElementsbyState();
 };
 
 /*
@@ -147,24 +210,63 @@ function logout(){
 
 /*
 	Implementation of the /user/register endpoint
+
+	//TODO: change the alerts in this function to something nicer, 
+	// like a shaking window or a shaking text box or something.
+
+	//TODO: some sort of simple input validation. the server is 
+	// validating inputs anyway, but no need to send garbage on purpose.
 */
 
-function userRegister(){
+function register(){
 	var request = defaultRequest;
 	request.auth.sid = Cookies.get('sid');
 
-	request.data.firstName=$('#registerfirstname').val();
-	request.data.lastName=$('#registerlastname').val();
-	request.data.email=$('#registeremail').val();
-	request.data.birthdate=$('#registerbirthdate').val();
-	request.data.address=$('#registeraddress').val();
-	request.data.password=$('#registerpassword').val();
+	var empty = "0";
 
-	$.postJSON(`${serverUrl}/user/register`,
-	request, 
-	function(response) {
-		// no response
-	});
+	if (! $('#frm-email').val() || ! $('#frm-pass').val() ) {
+		//alert("password or email empty");
+		empty = 1;
+	}
+
+	request.data.email=$('#frm-email').val();
+	request.data.password=$('#frm-pass').val();
+
+	if (! $('#frm-firstname').val() || ! $('#frm-lastname').val() ) {
+		//alert("name not entered correctly");
+		empty = 1;
+	}
+
+	request.data.firstName=$('#frm-firstname').val();
+	request.data.lastName=$('#frm-lastname').val();
+
+	if (! $('#frm-address').val() || ! $('#frm-birthday').val() ) {
+		//alert("address of birthday empty");
+		empty = 1;
+	}
+
+	request.data.address=$('#frm-address').val();
+	request.data.birthday=$('#frm-birthday').val();
+
+	if (! $('#cbx-terms').is(':checked') ) {
+		alert("Please accept our terms!");
+	}
+
+	if (! empty && $('#cbx-terms').is(':checked') ) {
+		$.postJSON(`${serverUrl}/user/register`,
+		request, 
+		function(response) {
+			//TODO: Show a window, that registering was successful. 
+			// then ask the user to click a button to reload, that
+			// redirects to the login screen.
+
+			if (response.status.code == "200") {
+				loadMainPage();
+			}
+		});
+	} else {
+		console.log("fields incorrect, try again.");
+	}
 }
 
 /*
@@ -178,7 +280,7 @@ function userUpdate(){
 	request.data.firstName=$('#updatefirstname').val();
 	request.data.lastName=$('#updatelastname').val();
 	request.data.email=$('#updateemail').val();
-	request.data.birthdate=$('#updatebirthdate').val();
+	request.data.birthday=$('#updatebirthday').val();
 	request.data.address=$('#updateaddress').val();
 	request.data.password=$('#updatepassword').val();
 
@@ -188,7 +290,6 @@ function userUpdate(){
 		// no response
 	});
 }
-
 
 /* 
 	Implementation of the /room/list endpoint
@@ -200,7 +301,7 @@ function getRooms(){
 		for (var i = 0; i < response.data.rooms.length; i++){
 			$('#roomlist').append('<tr><td>' + response.data.rooms[i] + '</td></tr>');
 		}
-		});
+	});
 }
 
 /* 
@@ -282,8 +383,11 @@ function updateRoomInfo(){
 	$('#updateroomfeatureslist').find("tr:gt(0)").remove(); //remove all tr elements greater than 0
 
 	$('#updateroomid').prop('disabled' , false);
-
 }
+
+///////////////////////////////
+/////// event listeners ///////
+///////////////////////////////
 
 $( document ).ready(function(){ //only run this script after the loading of the page finished
 
@@ -291,20 +395,19 @@ $( document ).ready(function(){ //only run this script after the loading of the 
 	// not yet correctly implemented functions
 	$('#usergetinfo').click(getUserInfo); 	/* /user */
 	$('#update').click(userUpdate); 		/* /user/update */
-	$('#register').click(userRegister); 	/* /user/register */
 
 	$('#getroomlist').click(getRooms); 		/* /room/list */
 	$('#checkroomidsubmit').click(getRoomInfo); /* /room/# */
 	$('#updateroomidsubmit').click(updateRoomInfo); /* /room/update */
 	$('#featurefillroominfo').click(getRoomInfoUpdate); /* helper */
-	
-
 
 	// integrated functions
 
 	// event listeners for the functionality of the page
 	$('#btn-login').click(login);			/* /user/auth */
 	$('#btn-logout').click(logout);			/* /user/logout */
+	$('#btn-register').click(register);		/* /user/register */
+	$('#btn-usersettings').click(loadSettings);
 
 	// show hotel info in the footer
 	getHotelInfo();							/* /hotel */
