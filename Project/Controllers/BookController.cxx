56b69a5d837d297987f281caab1d12d565edccfa
Dominik Simon <admin@dsimon.xyz>
2020-02-05 13:27:53 +0100

    added BookController

---
+++ Project/Controllers/BookController.cxx
@@ -0,0 +1,25 @@
+#include "BookController.hxx"
+
+void BookController::create(shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request, json content){
+
+}
+
+void BookController::get(shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request, json content){
+
+}
+
+void BookController::list(shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request, json content){
+
+}
+
+void BookController::update(shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request, json content){
+
+}
+
+void BookController::del(shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request, json content){
+
+}
+
+void BookController::check(shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request, json content){
+
+}
---
+++ Project/Controllers/BookController.hxx
@@ -0,0 +1,32 @@
+#ifndef BOOK_CONTROLLER_HXX
+#define BOOK_CONTROLLER_HXX
+
+#include "server_http.hpp"
+
+#define BOOST_SPIRIT_THREADSAFE
+
+#include <algorithm>
+#include <boost/filesystem.hpp>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <nlohmann/json.hpp>
+
+using namespace std;
+using namespace SimpleWeb;
+using json = nlohmann::json;
+using HttpServer = SimpleWeb::Server<SimpleWeb::HTTP>;
+
+
+class BookController {
+    public:
+        BookController() {};
+        static void create(shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request, json content);
+        static void get(shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request, json content);
+        static void list(shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request, json content);
+        static void update(shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request, json content);
+        static void del(shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request, json content);
+        static void check(shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request, json content);
+};
+
+#endif
