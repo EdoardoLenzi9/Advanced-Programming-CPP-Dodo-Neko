2774be2339462eebabe67dd095a79b9e9f36915a
Edoardo Lenzi <edoardolenzi9@gmail.com>
2020-02-06 10:22:06 +0100

    delete session

--- Project/Services/AuthorizationService.cxx
+++ Project/Services/AuthorizationService.cxx
@@ -1,18 +1,11 @@
-#include <string>
-#include <iostream>
-#include <time.h>
-
 #include "AuthorizationService.hxx"
-#include "Repository.hxx"
-#include "sha256.hxx"
-#include "DtoException.hxx"
-#include "Const.hxx"
 
 
 string AuthorizationService::createSession(long user_id_){
     Authorization* auth = au->create(new Authorization(user_id_, sha256(to_string(clock()))));
     return auth->session_id();
 }
+
 
 long AuthorizationService::getSession(string session_id){
 
@@ -23,4 +16,14 @@ long AuthorizationService::getSession(string session_id){
     }
     
     throw new DtoException(Code::Unauthorized, UNAUTHORIZED);
+}
+
+
+void AuthorizationService::deleteSession(string session_id){
+
+    vector<Authorization> sessions = au->read(odb::query<Authorization>::session_id == session_id);
+
+    for(Authorization session : sessions){
+        au->del(session.id());
+    }
 }
