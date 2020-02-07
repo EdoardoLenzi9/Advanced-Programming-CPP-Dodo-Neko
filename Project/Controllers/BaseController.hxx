b9d04b6f7074297730bef462c1f72bf0d5eaadc0
Edoardo Lenzi <edoardolenzi9@gmail.com>
2020-02-07 14:19:13 +0100

    wip

--- Project/Controllers/BaseController.hxx
+++ Project/Controllers/BaseController.hxx
@@ -14,9 +14,9 @@ class BaseController{
     public:
         string serialize(json res){
             string statusCode = to_string(res["status"]["code"]);
-            string statusCodeLabel = statusCodeLabel[res["status"]["code"]];
+            string statusCodeLabel = statusCodeLabel[to_string(res["status"]["code"])];
 
-            return "HTTP/1.1 " + statusCode + " " + statusCodeLabel +"\r\n" + "Content-Length: " << res.dump().length() << "\r\n" + "Content-Type: application/json\r\n\r\n" + res.dump();
+            return "HTTP/1.1 " + statusCode + " " + statusCodeLabel +"\r\n" + "Content-Length: " + to_string(res.dump().length()) + "\r\n" + "Content-Type: application/json\r\n\r\n" + res.dump();
         }
 };
 
