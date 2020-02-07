ab225cc8d5ef317afbbeb1e528b8ac5b9f7bfa54
Dominik Simon <admin@dsimon.xyz>
2020-02-07 14:44:47 +0100

    fix static context

--- Project/Controllers/BaseController.hxx
+++ Project/Controllers/BaseController.hxx
@@ -1,20 +1,21 @@
 #ifndef BASE_CONTROLLER_HXX
 #define BASE_CONTROLLER_HXX
 
-using namespace std;
-using json = nlohmann::json;
 
 #include <string>
 #include <nlohmann/json.hpp>
+
+using namespace std;
+using json = nlohmann::json;
 
 #include "Const.hxx"
 
 class BaseController{
 
     public:
-        string serialize(json res){
-            string statusCode = to_string(res["status"]["code"]);
-            string statusCodeLabel = statusCodeLabel[to_string(res["status"]["code"])];
+        static string serialize(json res){
+            string statusCode = res["status"]["code"].get<string>();
+            string statusCodeLabel = CodeLabels[statusCode];
 
             return "HTTP/1.1 " + statusCode + " " + statusCodeLabel +"\r\n" + "Content-Length: " + to_string(res.dump().length()) + "\r\n" + "Content-Type: application/json\r\n\r\n" + res.dump();
         }
